ee1d2d41f0178333395af0b146b3c6f175d13b4c
Nikita <Nikita@ASUS>
2024-04-19 20:07:32 +0700

    Добавьте файлы проекта.

---
+++ Lab_5/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace Lab_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.initTextBox = new System.Windows.Forms.TextBox();
+            this.resultTextBox = new System.Windows.Forms.TextBox();
+            this.openButton = new System.Windows.Forms.Button();
+            this.generateButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.filterButton = new System.Windows.Forms.Button();
+            this.filterTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // initTextBox
+            // 
+            this.initTextBox.AcceptsTab = true;
+            this.initTextBox.Location = new System.Drawing.Point(16, 30);
+            this.initTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.initTextBox.Multiline = true;
+            this.initTextBox.Name = "initTextBox";
+            this.initTextBox.Size = new System.Drawing.Size(396, 451);
+            this.initTextBox.TabIndex = 0;
+            // 
+            // resultTextBox
+            // 
+            this.resultTextBox.AcceptsTab = true;
+            this.resultTextBox.Location = new System.Drawing.Point(439, 30);
+            this.resultTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.resultTextBox.Multiline = true;
+            this.resultTextBox.Name = "resultTextBox";
+            this.resultTextBox.Size = new System.Drawing.Size(396, 451);
+            this.resultTextBox.TabIndex = 1;
+            this.resultTextBox.TextChanged += new System.EventHandler(this.resultTextBox_TextChanged);
+            // 
+            // openButton
+            // 
+            this.openButton.Location = new System.Drawing.Point(16, 490);
+            this.openButton.Margin = new System.Windows.Forms.Padding(4);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(100, 28);
+            this.openButton.TabIndex = 2;
+            this.openButton.Text = "Открыть";
+            this.openButton.UseVisualStyleBackColor = true;
+            this.openButton.Click += new System.EventHandler(this.openButton_Click);
+            // 
+            // generateButton
+            // 
+            this.generateButton.Location = new System.Drawing.Point(124, 490);
+            this.generateButton.Margin = new System.Windows.Forms.Padding(4);
+            this.generateButton.Name = "generateButton";
+            this.generateButton.Size = new System.Drawing.Size(181, 28);
+            this.generateButton.TabIndex = 3;
+            this.generateButton.Text = "Генерация";
+            this.generateButton.UseVisualStyleBackColor = true;
+            this.generateButton.Click += new System.EventHandler(this.generateButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(313, 490);
+            this.clearButton.Margin = new System.Windows.Forms.Padding(4);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(100, 28);
+            this.clearButton.TabIndex = 4;
+            this.clearButton.Text = "Очистить";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(439, 490);
+            this.saveButton.Margin = new System.Windows.Forms.Padding(4);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(100, 28);
+            this.saveButton.TabIndex = 5;
+            this.saveButton.Text = "Сохранить";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // filterButton
+            // 
+            this.filterButton.Location = new System.Drawing.Point(719, 490);
+            this.filterButton.Margin = new System.Windows.Forms.Padding(4);
+            this.filterButton.Name = "filterButton";
+            this.filterButton.Size = new System.Drawing.Size(116, 28);
+            this.filterButton.TabIndex = 6;
+            this.filterButton.Text = "Фильтрация";
+            this.filterButton.UseVisualStyleBackColor = true;
+            this.filterButton.Click += new System.EventHandler(this.filterButton_Click);
+            // 
+            // filterTextBox
+            // 
+            this.filterTextBox.Location = new System.Drawing.Point(547, 490);
+            this.filterTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.filterTextBox.Name = "filterTextBox";
+            this.filterTextBox.Size = new System.Drawing.Size(163, 22);
+            this.filterTextBox.TabIndex = 7;
+            this.filterTextBox.TextChanged += new System.EventHandler(this.filterTextBox_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(848, 523);
+            this.Controls.Add(this.filterTextBox);
+            this.Controls.Add(this.filterButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.generateButton);
+            this.Controls.Add(this.openButton);
+            this.Controls.Add(this.resultTextBox);
+            this.Controls.Add(this.initTextBox);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximumSize = new System.Drawing.Size(866, 570);
+            this.MinimumSize = new System.Drawing.Size(866, 570);
+            this.Name = "Form1";
+            this.Text = "Маршруты";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox initTextBox;
+        private System.Windows.Forms.TextBox resultTextBox;
+        private System.Windows.Forms.Button openButton;
+        private System.Windows.Forms.Button generateButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button filterButton;
+        private System.Windows.Forms.TextBox filterTextBox;
+    }
+}
+
---
+++ Lab_5/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Lab_5 // Объявление пространства имен для вашего проекта.
+{
+    public struct MARSH // Определение структуры для хранения данных о маршруте.
+    {
+        public string StartPoint; // Начальный пункт маршрута.
+        public string EndPoint; // Конечный пункт маршрута.
+        public string RouteNumber; // Номер маршрута.
+
+        public override string ToString() // Метод для возвращения строкового представления маршрута.
+        {
+            return $"Маршрут {RouteNumber}: {StartPoint} - {EndPoint}"; // Форматированный вывод данных маршрута.
+        }
+    }
+
+    public partial class Form1 : Form // Определение класса формы.
+    {
+        private List<MARSH> marshes = new List<MARSH>(); // Список для хранения маршрутов.
+        private List<string> cities = new List<string>
+        {
+            "Москва", "Санкт-Петербург", "Новосибирск", "Екатеринбург",
+            "Казань", "Нижний Новгород", "Челябинск", "Самара",
+             "Омск", "Ростов-на-Дону", "Томск", "Кемерово", "Юрга"
+        };
+
+        public Form1() // Конструктор класса формы.
+        {
+            InitializeComponent(); // Инициализация компонентов формы.
+        }
+
+        // Метод, вызываемый при нажатии на кнопку "Открыть".
+        private void openButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog(); // Создание диалога для открытия файла.
+            openFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*"; // Установка фильтра для файлов.
+            openFileDialog.Title = "Открыть файл маршрутов"; // Установка заголовка диалога.
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK) // Если пользователь выбрал файл и нажал "OK".
+            {
+                marshes.Clear(); // Очистка списка маршрутов.
+                using (StreamReader sr = new StreamReader(openFileDialog.FileName)) // Создание потока чтения из файла.
+                {
+                    string line; // Переменная для хранения считываемой строки.
+                    while ((line = sr.ReadLine()) != null) // Чтение файла построчно.
+                    {
+                        var parts = line.Split(';'); // Разделение строки на части.
+                        if (parts.Length == 3) // Если строка содержит три части.
+                        {
+                            marshes.Add(new MARSH // Добавление нового маршрута в список.
+                            {
+                                StartPoint = parts[0],
+                                EndPoint = parts[1],
+                               RouteNumber = parts[2]
+                            });
+                        }
+                    }
+                }
+
+                initTextBox.Text = string.Join(Environment.NewLine, marshes); // Отображение маршрутов в текстовом поле.
+            }
+        }
+
+        // Метод, вызываемый при нажатии на кнопку "Сгенерировать".
+        private void generateButton_Click(object sender, EventArgs e)
+        {
+            Random random = new Random(); // Создание объекта для генерации случайных чисел.
+            marshes.Clear(); // Очистка списка маршрутов.
+
+            for (int i = 0; i < 10; i++) // Генерация десяти маршрутов.
+            {
+                var startPoint = cities[random.Next(cities.Count)]; // Выбор случайного начального города.
+                var endPoint = cities[random.Next(cities.Count)]; // Выбор случайного конечного города.
+                while (endPoint == startPoint) // Убедиться, что начальный и конечный города не совпадают.
+                {
+                    endPoint = cities[random.Next(cities.Count)];
+                }
+
+                marshes.Add(new MARSH // Добавление нового маршрута в список.
+                {
+                    StartPoint = startPoint,
+                    EndPoint = endPoint,
+                    RouteNumber = (i + 1).ToString()
+                });
+            }
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog(); // Создание диалога для сохранения файла.
+            saveFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*"; // Установка фильтра для файлов.
+            saveFileDialog.Title = "Сохранить сгенерированные данные"; // Установка заголовка диалога.
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK) // Если пользователь выбрал место для сохранения и нажал "OK".
+            {
+                using (StreamWriter sw = new StreamWriter(saveFileDialog.FileName)) // Создание потока записи в файл.
+                {
+                    foreach (var marsh in marshes) // Перебор всех маршрутов для записи.
+                    {
+                        sw.WriteLine($"{marsh.StartPoint};{marsh.EndPoint};{marsh.RouteNumber}"); // Запись маршрута в файл.
+                    }
+                }
+
+                MessageBox.Show("Данные сгенерированы и сохранены."); // Вывод сообщения об успешном сохранении.
+            }
+        }
+
+        // Метод, вызываемый при нажатии на кнопку "Очистить".
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            initTextBox.Clear(); // Очистка текстового поля с исходными данными.
+            resultTextBox.Clear(); // Очистка текстового поля с результатами.
+        }
+
+        // Метод, вызываемый при нажатии на кнопку "Сохранить".
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog(); // Создание диалога для сохранения файла.
+            saveFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*"; // Установка фильтра для файлов.
+            saveFileDialog.Title = "Сохранить результаты"; // Установка заголовка диалога.
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK) // Если пользователь выбрал место для сохранения и нажал "OK".
+            {
+                File.WriteAllText(saveFileDialog.FileName, resultTextBox.Text); // Запись содержимого текстового поля в файл.
+                MessageBox.Show("Результаты сохранены в файле: " + saveFileDialog.FileName); // Вывод сообщения об успешном сохранении.
+            }
+        }
+
+        // Метод, вызываемый при нажатии на кнопку "Фильтровать".
+        private void filterButton_Click(object sender, EventArgs e)
+        {   
+            string h1 = filterTextBox.Text; 
+            foreach(var marsh in marshes)
+            {
+                if (h1 == marsh.RouteNumber) { 
+                    resultTextBox.Text = marsh.ToString();
+                    }
+                
+                else if (Convert.ToInt32(h1) > 10)
+                {
+                    string t ="Такого маршрута нет ";
+                    resultTextBox.Text = t.ToString();
+                }
+            }
+        }
+        private void filterTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void resultTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
